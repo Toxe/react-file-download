2dbc35a259c8b2e2df637bc3cb5d8ad137723210
Toxe <toxe@toxe.net>
2020-09-08 17:52:53 +0200

    Download as blob

--- src/Link.js
+++ src/Link.js
@@ -1,14 +1,30 @@
 import React from "react";
 import styled from "styled-components";
+import axios from "axios";
+import { saveAs } from "file-saver";
 
 const Button = styled.button`
     margin: 3px;
 `;
 
 export default function Link({ filename }) {
+    const handleDownload = () => {
+        axios
+            .get(`/download/${filename}`, { responseType: "blob" })
+            .then((response) => {
+                console.log(
+                    `downloaded ${filename}: ${response.status} ${response.statusText}, ${response.headers["content-type"]}, ${response.data.size} bytes`
+                );
+                saveAs(response.data, filename);
+            })
+            .catch((error) => console.log(error));
+    };
+
     return (
         <div>
-            <Button type="button">{filename}</Button>
+            <Button type="button" onClick={handleDownload}>
+                {filename}
+            </Button>
         </div>
     );
 }
